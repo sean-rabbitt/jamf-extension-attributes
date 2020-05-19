2671e78102bade81c85e8df397fb1f2abf29036f
Sean Rabbitt <43422108+sean-rabbitt@users.noreply.github.com>
2020-05-19 13:30:16 -0400

    Create Add IDP Login as Alias to Local.sh
    A script to run via Jamf Pro policy, once per user, on any user who has Jamf Connect Login installed.  Will add an alias of their IDP login to the local login.  So if a network connection is down, user doesn't need to remember to login with either foo or foo@bar.com,  both will work.

---
+++ Add IDP Login as Alias to Local.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Add a local user login alias that matches the user's IDP login for Jamf Connect Login if 
+# network is offline.  Assumes that you're running this as a policy from Jamf Pro scripts.  
+# If not, change the definition of the LOCALUSER variable below to the alternative method.
+# — SRABBITT May 19, 2020 1:13 PM
+
+# MIT License
+#
+# Copyright (c) 2020 Jamf Software
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# $3 is obtained from Jamf Pro when running through policy or self service.
+LOCALUSER=$3
+
+# Alternative method - /bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'
+# LOCALUSER=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+
+# NETWORKUSER finds the "NetworkUser: " tag in a dscl lookup.
+NETWORKUSER=($(dscl . -read /Users/$LOCALUSER| grep "NetworkUser: " | awk {'print $2'}))
+
+# Sanity Check - If NETWORKUSER is empty, we have an issue.
+if [[ -z "$NETWORKUSER" ]]; then
+	# User is not migrated to Jamf Connect Login - abort
+	exit 1
+fi
+# Add alias to local user's login name.
+dscl . -merge /Users/$LOCALUSER RecordName $NETWORKUSER
+
+exit 0
